41e72be4b7c0e03a84321efd56eb3b0368f46935
gracez1412 <44016018+gracez1412@users.noreply.github.com>
2019-12-03 12:13:29 -0800

    underline subjects in polar sentence
    categories in footer

--- js/journal.js
+++ js/journal.js
@@ -1,15 +1,46 @@
 var num = 0;
 var write = document.getElementById('write');
 var popup = document.getElementById('popup');
+var footer = document.getElementById('clear_footer')
 var $highlights = $('.highlights');
 var $backdrop = $('.backdrop');
+var num_highlights = 0;
 
 var key, flag = 0;
 
-function applyHighlights(text, valence) {
+function applyHighlights(text, valence, cats, subject) {
     var edits = text;
-    if (valence == "pos") {edits = "<markpos>" + edits + "</markpos>";}
-    else if (valence == "neg") {edits = "<markneg>" + edits + "</markneg>";}
+    var subjects = subject.split(" ");
+    console.log(subjects);
+
+    console.log("val" + valence);
+    if (valence == "pos") {
+        num_highlights++;
+        var h_id = 'h_' + num_highlights;
+        var t_id = 't_' + num_highlights;
+        subjects.forEach(function (sub, i) {
+            var cut = edits.search(sub);
+            var len = sub.length;
+            edits = edits.substring(0, cut) + "<und_pos>" + edits.substring(cut, cut + len) + "</und_pos>" + edits.substring(cut+len);
+        });
+        edits = "<markpos id=" + (h_id) +" style='pointer-events:auto' onclick='showTags(" + num_highlights + ")'>" + edits + "</markpos>";
+        document.getElementById("popup").innerHTML = subject;
+        footerPopup(cats, num_highlights);
+
+    }
+    else if (valence == "neg") {
+        num_highlights++;
+        var h_id = 'h_' + num_highlights;
+        var t_id = 't_' + num_highlights;
+        subjects.forEach(function (sub, i) {
+            var cut = edits.search(sub);
+            var len = sub.length;
+            edits = edits.substring(0, cut) + "<und_neg>" + edits.substring(cut, cut + len) + "</und_neg>" + edits.substring(cut+len);
+        });
+        edits = "<markneg id=" + (h_id) +" style='pointer-events:auto' onclick='showTags(" + num_highlights + ")'>" + edits + "</markneg>";
+        document.getElementById("popup").innerHTML = subject;
+        footerPopup(cats, num_highlights);
+    }
     else { edits = "<marknut>" + edits + "</marknut>"; }
     
     return edits;
@@ -26,8 +57,40 @@ function runPyScript(input){
     return jqXHR.responseText;
 }
 
+function showTags(t_ID)
+{
+    for (i = 1; i <= num_highlights; i++)
+    {
+        tag = document.getElementById('t_'+i);
+        if (tag.style.display == "block") {tag.style.display = "none";}
+    }
+    console.log('click tags' + t_ID);
+    document.getElementById('t_' + t_ID).style.display = "block";
+
+    document.getElementById("write").style.pointerEvents = "auto";
+}
+
+function removeTags(t_ID)
+{
+    console.log("t_ID");
+    var x = document.getElementById(t_ID);
+    x.parentNode.removeChild(x);
+}
+
 write.onclick = function(){
-    if (popup.style.display == "block") { popup.style.display = "none"; return; }
+    // if (popup.style.display == "block") { popup.style.display = "none"; return; }
+    // while (footer.firstChild) { 
+    //     if (footer.firstChild.style.display == "block"){footer.firstChild.style.display == "none"}
+    // }
+    console.log("click!");
+    document.getElementById("write").style.pointerEvents = "none";
+    for (i = 1; i <= num_highlights; i++)
+    {
+        console.log('hiding t_' + i);
+        tag = document.getElementById('t_'+i);
+        if (tag.style.display == "block") { tag.style.display = "none"; }
+        console.log(tag.style.display);
+    }
 
     var curpos = write.selectionStart;
     var currhtml = $highlights.html();
@@ -44,6 +107,7 @@ write.onclick = function(){
                 var label = currhtml.slice(i+2, i+9);
 
                 if (label == "markneg"){ negPopup(); return; }
+                else if (label == "markpos") { negPopup(); return; }
                 else { return; }
             }
             else if(currhtml[i] == "<") { return; }
@@ -53,6 +117,36 @@ write.onclick = function(){
 
 function negPopup() {
     popup.style.display = "block";
+}
+
+function footerPopup(tags, num) {
+    for (i = 1; i <= num_highlights-1; i++)
+    {
+        tag = document.getElementById('t_'+i);
+        if (tag.style.display == "block") {tag.style.display = "none";}
+        console.log(tag.style.display);
+    }
+
+    t_id = 't_'+num_highlights;
+
+    var fancy_tags = tags.split(" ");
+    var all_tags=""
+
+    var tag_inner = "<div class='w3-container w3-tag w3-round w3-theme-l2' style='padding:3px 5px'><img src = 'https://findicons.com/files/icons/557/creme/128/delete.png' height='15' id="; 
+    var tag_mid = " onclick='removeTags("
+    var tag_func = ");'><div class='w3-container w3-tag w3-round w3-theme-l2 w3-border w3-border-white'>";
+    var tag_end = "</div></div>";
+
+    fancy_tags.forEach(function (tag, i) {
+        var id = num.toString() + tag;
+        all_tags += tag_inner + id + tag_mid + id + tag_func + tag + tag_end;
+    });
+
+    var tagify = "<div id=" + t_id + ">" + all_tags + "</div>";
+    document.querySelector('footer').innerHTML += (tagify);
+    document.getElementById(t_id).style.display = "none";
+
+    
 }
 
 write.onkeydown = function(event) {
@@ -93,12 +187,21 @@ write.oninput = function() {
 
     else {
         var text = write.value;
-        if (text[text.length - 1] == ".") {
+        if (text[text.length - 1] == "." || text[text.length - 1] == "?" || text[text.length - 1] == "!") {
             var result = runPyScript(text);
+            console.log("result" + result)
             var resparse = JSON.parse(result); 
+            console.log(resparse);
+
+            var cats = resparse["cats"];
+            var und = resparse["und"];
+            // var effect = resparse["empath"];
+            console.log(cats);
+            console.log(und);
 
             var slice = text.slice(resparse["start"],resparse["end"]);
-            var highlightedText = applyHighlights(slice, resparse["valence"]);
+            var highlightedText = applyHighlights(slice, resparse["valence"], cats, und);
+            console.log("hT" + highlightedText);
             if ($highlights.html() == "") {$highlights.html($highlights.html() + highlightedText);}
             else {$highlights.html($highlights.html() + " " + highlightedText);}
         }
@@ -124,3 +227,4 @@ write.onscroll = function() {
     var scrollTop = $('textarea').scrollTop();
     $backdrop.scrollTop(scrollTop);
 }
+
