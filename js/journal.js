af0cf4ad4525db6740dc7f4ef337cd3ebfa06071
gracez1412 <44016018+gracez1412@users.noreply.github.com>
2019-12-04 03:16:55 -0800

    smart autosuggest

--- js/journal.js
+++ js/journal.js
@@ -7,7 +7,29 @@ var $backdrop = $('.backdrop');
 var num_highlights = 0;
 
 var key, flag = 0;
+var curr_index = 0;
+var brcounter = 0;
 
+// Page Onload Operation
+write.onscroll = function() {
+    var scrollTop = $('textarea').scrollTop();
+    $backdrop.scrollTop(scrollTop);
+    console.log("ScrollTop");
+}
+
+if(window.localStorage["last_index"]){
+    curr_index = parseInt(window.localStorage["last_index"]);
+    if (!curr_index) {
+        curr_index = 1;
+    }
+}
+else{
+    window.localStorage["last_index"] = 1;
+}
+loaddata(curr_index);
+document.getElementById("m"+curr_index.toString()).style.background = "#d3d";
+
+// Support Functions
 function applyHighlights(text, valence, cats, subject) {
     var edits = text;
     var subjects = subject.split(" ");
@@ -26,6 +48,7 @@ function applyHighlights(text, valence, cats, subject) {
         edits = "<markpos id=" + (h_id) +" style='pointer-events:auto' onclick='showTags(" + num_highlights + ")'>" + edits + "</markpos>";
         document.getElementById("popup").innerHTML = subject;
         footerPopup(cats, num_highlights);
+        document.getElementById('')
 
     }
     else if (valence == "neg") {
@@ -70,12 +93,13 @@ function showTags(t_ID)
     document.getElementById("write").style.pointerEvents = "auto";
 }
 
-function removeTags(t_ID)
+function removeTags(t, ID)
 {
     console.log("t_ID");
-    var x = document.getElementById(t_ID);
-    x.parentNode.removeChild(x);
+    var x = document.getElementById(ID.toString()+ t.toString());
+    x.parentNode.parentNode.removeChild(x.parentNode);
 }
+
 
 write.onclick = function(){
     // if (popup.style.display == "block") { popup.style.display = "none"; return; }
@@ -83,7 +107,7 @@ write.onclick = function(){
     //     if (footer.firstChild.style.display == "block"){footer.firstChild.style.display == "none"}
     // }
     console.log("click!");
-    document.getElementById("write").style.pointerEvents = "none";
+    
     for (i = 1; i <= num_highlights; i++)
     {
         console.log('hiding t_' + i);
@@ -115,6 +139,11 @@ write.onclick = function(){
     }
 }
 
+write.ondblclick = function() {
+    console.log("double-click!");
+    document.getElementById("write").style.pointerEvents = "none";
+}
+
 function negPopup() {
     popup.style.display = "block";
 }
@@ -133,26 +162,26 @@ function footerPopup(tags, num) {
     var all_tags=""
 
     var tag_inner = "<div class='w3-container w3-tag w3-round w3-theme-l2' style='padding:3px 5px'><img src = 'https://findicons.com/files/icons/557/creme/128/delete.png' height='15' id="; 
-    var tag_mid = " onclick='removeTags("
-    var tag_func = ");'><div class='w3-container w3-tag w3-round w3-theme-l2 w3-border w3-border-white'>";
+    var tag_mid = " onclick='removeTags(\"";
+    var tag_func = ")'><div class='w3-container w3-tag w3-round w3-theme-l2 w3-border w3-border-white'>";
     var tag_end = "</div></div>";
 
     fancy_tags.forEach(function (tag, i) {
-        var id = num.toString() + tag;
-        all_tags += tag_inner + id + tag_mid + id + tag_func + tag + tag_end;
+        var id = num.toString() + tag.toString();
+        all_tags += tag_inner + id + tag_mid + tag +"\"," + num.toString() + tag_func + tag + tag_end;
     });
 
     var tagify = "<div id=" + t_id + ">" + all_tags + "</div>";
     document.querySelector('footer').innerHTML += (tagify);
     document.getElementById(t_id).style.display = "none";
-
-    
 }
 
 write.onkeydown = function(event) {
     key = event.keyCode;
+    //console.log(key)
 }
 
+// Process input text.
 write.oninput = function() {
     var currhtml = $highlights.html();
     currhtml = String(currhtml);
@@ -163,7 +192,11 @@ write.oninput = function() {
         $highlights.html(currhtml.slice(0, n));
     }
 
-    if (key == 8 || key == 46) {    // Backspace + Delete
+    if (key == 13){    // LF
+        $highlights.html($highlights.html() + "<marknut><br></marknut>");
+        brcounter += 1;
+    }
+    else if (key == 8 || key == 46) {    // Backspace + Delete
         var numsent_wr = (write.value.match(/\./g)||[]).length;
         var numsent_ht = (currhtml.match(/\./g)||[]).length;
 
@@ -181,50 +214,159 @@ write.oninput = function() {
                 }
             }
         }
-        
         return;
     }
-
     else {
         var text = write.value;
-        if (text[text.length - 1] == "." || text[text.length - 1] == "?" || text[text.length - 1] == "!") {
-            var result = runPyScript(text);
-            console.log("result" + result)
-            var resparse = JSON.parse(result); 
-            console.log(resparse);
+        var result = runPyScript(text);
+        console.log("result" + result)
+        var resparse = JSON.parse(result); 
+        console.log(resparse);
 
-            var cats = resparse["cats"];
-            var und = resparse["und"];
-            // var effect = resparse["empath"];
-            console.log(cats);
-            console.log(und);
+        var cats = resparse["cats"];
+        var und = resparse["und"];
+        var eliza = resparse["eliza"];
+        // var effect = resparse["empath"];
+        console.log(cats);
+        console.log(und);
+        
+        if (text[text.length - 1] == "." || text[text.length - 1] == "?" || text[text.length - 1] == "!") {
 
             var slice = text.slice(resparse["start"],resparse["end"]);
             var highlightedText = applyHighlights(slice, resparse["valence"], cats, und);
             console.log("hT" + highlightedText);
             if ($highlights.html() == "") {$highlights.html($highlights.html() + highlightedText);}
+            else if (brcounter > 0) {brcounter = 0; $highlights.html($highlights.html() + highlightedText);}    // if LF, no extra space is needed.
             else {$highlights.html($highlights.html() + " " + highlightedText);}
         }
 
-        else if (text[text.length - 1] == " " && text[text.length - 2] == ".") {
+        else if (text[text.length - 1] == " " && (text[text.length - 2] == "." || text[text.length - 2] == "?" || text[text.length - 2] == "!")) {
             var i;
             for (i = currhtml.length - 1; i >= 0; i--) {
                 if (currhtml[i] == "/") {
                     var label = currhtml.slice(i+1, i+8);
-                    if (label == "markneg") {
-                        $highlights.html($highlights.html() + " <marktmp>Try reframing that negative thought.</marktmp>");
+                    if (label == "markneg" || label == "markpos") {
+                        $highlights.html($highlights.html() + " <marktmp>" + eliza + "</marktmp>");
                         flag = 1;
                     }
                     return;
                 }
-
             }
         }
     }
 }
 
-write.onscroll = function() {
-    var scrollTop = $('textarea').scrollTop();
-    $backdrop.scrollTop(scrollTop);
+function swich(i){
+    document.getElementById("m" + curr_index.toString()).style.background = "#a34";
+    document.getElementById("m"+i.toString()).style.background = "#d3d";
+    savedata_noalert();
+    curr_index = i;
+    loaddata(curr_index);
+    $highlights.html("");
+    console.log("Switch to ", i);
 }
 
+function loaddata(index){
+    var name = "data" + index.toString();
+    if (index == -1){
+        name = "data" + curr_index.toString()
+    }
+    else if (window.localStorage[name]){
+        document.getElementById("write").value = window.localStorage[name];
+    } 
+    else{
+        document.getElementById("write").value = ""
+    }
+    console.log("loaddata",name)
+}
+
+function savedata(){
+    var name = "data" + curr_index.toString();
+    console.log("save",name)
+    window.localStorage[name] = document.getElementById("write").value;
+    window.localStorage["last_index"] = curr_index;
+    alert('All Secure!');
+}
+
+function cleardata(){
+    window.localStorage["withdraw"] = document.getElementById("write").value;
+    document.getElementById("write").value = "";
+    savedata_noalert();
+    $highlights.html("");
+
+    var footer = document.getElementById("clear_footer");
+	while (footer.firstChild) footer.removeChild(footer.firstChild);
+}
+
+function withdraw(){
+    if(window.localStorage["withdraw"]){
+        document.getElementById("write").value = window.localStorage["withdraw"];
+    }
+    else{
+        alert('No input can be withdrawn!');
+    }
+}
+
+function savedata_noalert(){
+    var name = "data" + curr_index.toString();
+    window.localStorage[name] = document.getElementById("write").value;
+    window.localStorage["last_index"] = curr_index;
+}
+
+/*
+//code copy from http://stackoverflow.com/questions/1601593/fire-tab-keypress-event-in-javascript
+document.getElementById('write').onkeydown = function(e){
+    if (e.keyCode == 9) {
+        insertAtCursor('    ');
+        return false;
+    }
+}
+
+// code edit from http://jsfiddle.net/Znarkus/Z99mK/
+function insertAtCursor(myValue) {
+    myField = document.getElementById("write");
+    //IE support
+    if (document.selection) {
+        myField.focus();
+        sel = document.selection.createRange();
+        sel.text = myValue;
+    }
+    //MOZILLA and others
+    else if (myField.selectionStart || myField.selectionStart == '0') {
+        var startPos = myField.selectionStart;
+        var endPos = myField.selectionEnd;
+        myField.value = myField.value.substring(0, startPos)
+            + myValue
+            + myField.value.substring(endPos, myField.value.length);
+        myField.selectionStart = startPos + myValue.length;
+        myField.selectionEnd = startPos + myValue.length;
+    } else {
+        myField.value += myValue;
+    }
+}
+*/
+function mood(){
+    $(".dislog").css("display","block");
+}
+
+function mood_hide(){
+    $(".dislog").css("display","none");
+}
+
+function left(){
+    $(".dislog").css("display","none");
+    $highlights.html(" <marktmp>Sorry to hear that. Wanna talk about what happened?</marktmp>");
+    flag = 1;
+}
+
+function middle(){
+    $(".dislog").css("display","none");
+    $highlights.html(" <marktmp>Any plan tomorrow?</marktmp>");
+    flag = 1;
+}
+
+function right(){
+    $(".dislog").css("display","none");
+    $highlights.html(" <marktmp>Cool! Why not write down those happy moments now?</marktmp>");
+    flag = 1;
+}
